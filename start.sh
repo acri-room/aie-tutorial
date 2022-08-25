1af4d3b2d4f3eda9ae382608ac3823385e5de4df
Jun Ando <jun.ando@amd.com>
2022-08-25 22:20:30 +0900

    Updated to 2022.1

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-source /tools/Xilinx/Vitis/2021.2/settings64.sh
+source /tools/Xilinx/Vitis/2022.1/settings64.sh
 source /opt/xilinx/xrt/setup.sh
 
 jupyter lab --ip 127.0.0.1
