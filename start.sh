f2a338fcd87435c9c3cb7d1755471672b5be1db7
Jun Ando <jun.ando@amd.com>
2023-04-13 18:27:04 +0900

    Updated tool and platform to 2022.2

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
-source /tools/Xilinx/Vitis/2022.1/settings64.sh
+source /tools/Xilinx/Vitis/2022.2/settings64.sh
 source /opt/xilinx/xrt/setup.sh
+
+. /opt/conda/etc/profile.d/conda.sh
+conda activate
 
 jupyter lab --ip 127.0.0.1
