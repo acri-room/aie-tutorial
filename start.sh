41a7c0f0d255bcbe80d6e97de42eea0538b2ff51
Jun Ando <jun.ando@amd.com>
2023-07-10 22:37:04 +0900

    Updated for as005

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@ set -e
 source /tools/Xilinx/Vitis/2022.2/settings64.sh
 source /opt/xilinx/xrt/setup.sh
 
-. /opt/conda/etc/profile.d/conda.sh
-conda activate
+if [[ -e /opt/conda/etc/profile.d/conda.sh ]] ; then
+    . /opt/conda/etc/profile.d/conda.sh
+    conda activate
+fi
 
 jupyter lab --ip 127.0.0.1
